93099f8df767bec7b6ab46869fa3199826e1707d
Devarsh shukla <Devarsh14@gmail.com>
2019-03-19 21:44:38 +1000

    Unit testing with moq added.

---
+++ DesignPatterns/ProblemSolving/MaximumDrawers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.ProblemSolving
+{
+    public  class MaximumDrawers
+    {
+        private static int MaximumDraws(int n)
+        {
+            /*
+             * Write your code here.
+             */
+            return n;
+        }
+    }
+}
