92ee1232b4604603d62a41528295491e40e63af6
Devarsh shukla <Devarsh14@gmail.com>
2019-03-26 09:48:40 +1000

    Time conversion from AMORPM to military time is implemented.

---
+++ DesignPatterns/Algorithms/HacherRank/TimeConversion.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Algorithms.HacherRank
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class TimeConversion
+    {
+        public string TimeConversions(string timeConversion)
+        {
+            DateTime dateTime = DateTime.ParseExact(timeConversion, "hh:mm:sstt",
+                            System.Globalization.CultureInfo.InvariantCulture);
+
+            string convertedString = dateTime.ToString("HH:mm:ss");
+            return convertedString;
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,34 +1,33 @@
-﻿using DesignPatterns.CSharpFeatures;
-using DesignPatterns.ProblemSolving;
-using System;
+﻿namespace DesignPatterns
+{
+    using System;
+    using DesignPatterns.Algorithms.HacherRank;
+    using DesignPatterns.CSharpFeatures;
+    using DesignPatterns.ProblemSolving;
 
-namespace DesignPatterns
-{  
-    //https://refactoring.guru/design-patterns/factory-method
+    // https://refactoring.guru/design-patterns/factory-method
     // NAME OF EXTENTION IS SUPERCHARGER.
-    // BUG: 
-  // Creational patterns allows and provides ways to create objects in  application
+    // Install extention stylecop and also install extention stylecopAnalysers.
+    // BUG:
+    // Creational patterns allows and provides ways to create objects in  application
     public class Desing
     {
         public static void Main()
         {
-            //HashTables hashTables = new HashTables();
-            //hashTables.HashTableDemo();
+            SwitchConsoleStatements();
 
-            
-            Console.WriteLine("Select input for the execution");
-            Console.WriteLine("0 for MinMax value");
-            Console.WriteLine("1 for BirthDay candles");
             int[] arr;
+
             // Get input selected for class to execute.
             int.TryParse(Console.ReadLine(), out int n);
+
             switch (n)
             {
                 case 0:
                     Console.WriteLine("Please insert 5 integers with space separated.");
 
-                    // Min max sum 
-                    arr = ArrayInput();  
+                    // Min max sum
+                    arr = ArrayInput();
                     MiniMaxSum miniMaxSum = new MiniMaxSum();
                     miniMaxSum.MiniMaxsum(arr);
                     Console.Read();
@@ -43,24 +42,34 @@ namespace DesignPatterns
                     birthdayCackeCandles.BirthdayCakeCandles(arr);
                     Console.Read();
                     break;
+                case 2:
+                    Console.WriteLine("Insert time conversion string ex: 12:05:05PM");
+                    string timeString = Console.ReadLine();
+                    TimeConversion timeConversion = new TimeConversion();
+                    string convertedTime = timeConversion.TimeConversions(timeString);
+                    Console.WriteLine(convertedTime);
+                    Console.ReadLine();
+                    break;
                 default:
                     Console.WriteLine("Invalid input");
                     Console.ReadLine();
                     break;
             }
+        }
 
-
-
+        private static void SwitchConsoleStatements()
+        {
+            Console.WriteLine("Select input for the execution");
+            Console.WriteLine("0 for MinMax value");
+            Console.WriteLine("1 for BirthDay candles");
+            Console.WriteLine("2 for DateTimeConversion");
         }
 
         private static int[] ArrayInput()
         {
-            return Array.ConvertAll(Console.ReadLine().Split(' '),
+            return Array.ConvertAll(
+                Console.ReadLine().Split(' '),
                 arrTemp => Convert.ToInt32(arrTemp));
         }
     }
-    
 }
-
-
-
