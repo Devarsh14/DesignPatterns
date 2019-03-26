1e8ba7290af0310e4e24fc24ad2ccc5774d01ded
Devarsh shukla <Devarsh14@gmail.com>
2019-03-26 19:54:27 +1000

    Jagged array initialise.

---
+++ DesignPatterns/Algorithms/Challenge/MatrixInSpiralOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Algorithms.Challenge
+{
+    public class MatrixInSpiralOrder
+    {
+        public void PrintMatrixInSprialOrder()
+        {
+            Console.WriteLine("Write matarix");
+
+            int[][] mat = new int[][]
+        {
+            new int[] { 1,  2,  3,  4, 5},
+            new int[] {16, 17, 18, 19, 6},
+            new int[] {15, 24, 25, 20, 7},
+            new int[] {14, 23, 22, 21, 8},
+            new int[] {13, 12, 11, 10, 9}
+        };
+
+        }
+    }
+}
